0c6c7949b4dbd16da67ee766b4e93e857acf9acd
puly garcia <pulygarcia09@gmail.com>
2023-10-13 17:49:12 -0300

    adding /varieties, iterating

---
+++ src/data/varietiesData.js
@@ -0,0 +1,62 @@
+export const varieties = [
+    {
+        name: "California",
+        ingredients: ['chocolate', 'crema','chocolate', 'crema'],
+        img: '/1.jpg',
+    },
+    {
+        name: "SPQ",
+        ingredients: ['chocolate', 'crema'],
+        img: '/2.jpg',
+    },
+    {
+        name: "Buenos aires",
+        ingredients: ['chocolate', 'crema'],
+        img: '/3.jpg',
+    },
+    {
+        name: "California",
+        ingredients: ['chocolate', 'crema','chocolate', 'crema'],
+        img: '/1.jpg',
+    },
+    {
+        name: "SPQ",
+        ingredients: ['chocolate', 'crema'],
+        img: '/2.jpg',
+    },
+    {
+        name: "Buenos aires",
+        ingredients: ['chocolate', 'crema'],
+        img: '/3.jpg',
+    },
+    {
+        name: "California",
+        ingredients: ['chocolate', 'crema','chocolate', 'crema'],
+        img: '/1.jpg',
+    },
+    {
+        name: "SPQ",
+        ingredients: ['chocolate', 'crema'],
+        img: '/2.jpg',
+    },
+    {
+        name: "Buenos aires",
+        ingredients: ['chocolate', 'crema'],
+        img: '/3.jpg',
+    },
+    {
+        name: "California",
+        ingredients: ['chocolate', 'crema','chocolate', 'crema'],
+        img: '/1.jpg',
+    },
+    {
+        name: "SPQ",
+        ingredients: ['chocolate', 'crema'],
+        img: '/2.jpg',
+    },
+    {
+        name: "Buenos aires",
+        ingredients: ['chocolate', 'crema'],
+        img: '/3.jpg',
+    },
+]
